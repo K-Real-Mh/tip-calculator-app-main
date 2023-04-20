81a62321a8cf79f4b4c0098a1e617ad3c80d0c45
Kirill Makhov <mmahovkirill@gmail.com>
2023-04-21 00:45:07 +0300

    feat: add rules to eslint and add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,11 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
@@ -12,4 +17,4 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
-}
+};
