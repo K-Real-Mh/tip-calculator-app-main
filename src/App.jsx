4110e047270d9b82c2753568e2ba9728928a23cd
Kirill Makhov <mmahovkirill@gmail.com>
2023-04-22 22:59:24 +0300

    refactor: add Icon and TipCalculator container

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { TipCalculator } from './containers';
+import './App.css';
+import { Icon } from './components/index';
 
 function App() {
-  return <>Frontend Mentor | Tip calculator app</>;
+  return (
+    <div className="App">
+      <Icon name="LogoIcon" width="87" height="54" color="var(--logo-icon-cyan)" className="App__icon" />
+      <TipCalculator />
+    </div>
+  );
 }
 
 export default App;
