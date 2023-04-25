4462621930b504a8d399ada2804d1d98abf3d625
Kirill Makhov <mmahovkirill@gmail.com>
2023-04-25 23:02:39 +0300

    feat: create constants for TipCalculator container

---
+++ src/containers/TipCalculator/consts.js
@@ -0,0 +1,16 @@
+export const BLOCK_TYPES = {
+  BILL: 'BILL',
+  PERCENTAGE: 'PERCENTAGE',
+  PEOPLE: 'PEOPLE',
+  TIP: 'TIP',
+  TOTAL: 'TOTAL',
+  RESET: 'RESET',
+}
+
+export const RADIOS = [
+  { value: '5', label: '5%' },
+  { value: '10', label: '10%' },
+  { value: '15', label: '15%' },
+  { value: '25', label: '25%' },
+  { value: '50', label: '50%' },
+]
