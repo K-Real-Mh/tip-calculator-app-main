25e02e75dab2c74dd7129e9ffdd7896e51faabae
Kirill Makhov <mmahovkirill@gmail.com>
2023-04-26 22:55:07 +0300

    refactor: action cases

--- src/containers/TipCalculator/context/StateProvider.jsx
+++ src/containers/TipCalculator/context/StateProvider.jsx
@@ -1,6 +1,14 @@
 import React, { useReducer } from 'react';
 import { initialState, store } from './store';
 import { RESET, UPDATE_BILL, UPDATE_PEOPLE, UPDATE_PERCENTAGE } from './actions';
+import {
+  calculateTipAndTotal,
+  isValidPayload,
+  removeLeadingZero,
+  validateBill,
+  validatePeople,
+  validatePercentage,
+} from './helpers';
 
 const { Provider } = store;
 
@@ -8,42 +16,52 @@ function StateProvider({ children }) {
   const [state, dispatch] = useReducer((prevState, action) => {
     const { type, payload } = action;
 
+    const getUpdatedState = (updateFunc) => {
+      const { bill, percentage, people } = prevState;
+      const updatedValue = removeLeadingZero(payload);
+
+      return {
+        ...prevState,
+        ...updateFunc(updatedValue, bill, percentage, people),
+      };
+    }
+
     switch (type) {
       case UPDATE_BILL: {
-        const { people, percentage } = prevState;
+        if (!isValidPayload(payload, validateBill)) {
+          return { ...prevState };
+        }
 
-        const total = people === 0 ? 0 : payload / people;
-        const tip = total * (percentage / 100);
-
-        return {
-          ...prevState,
-          bill: payload,
-          tip: tip.toFixed(2),
-          total: total.toFixed(2),
-        };
+        return getUpdatedState((bill, _, percentage, people) =>
+          calculateTipAndTotal(bill, people, percentage)
+        );
       }
       case UPDATE_PERCENTAGE: {
-        const { bill, people } = prevState;
+        if (!isValidPayload(payload, validatePercentage)) {
+          return { ...prevState };
+        }
 
-        const total = people === 0 ? 0 : bill / people;
-        const tip = total * (payload / 100);
+        return getUpdatedState((percentage, bill, _, people) =>
+          calculateTipAndTotal(bill, people, percentage)
+        );
+      }
+      case UPDATE_PEOPLE: {
+        if (!isValidPayload(payload, validatePeople)) {
+          return { ...prevState };
+        }
+        const peopleValue = removeLeadingZero(payload);
 
         return {
           ...prevState,
-          percentage: payload,
-          tip: tip.toFixed(2),
-          total: total.toFixed(2),
+          people: peopleValue,
+          isPeopleError: peopleValue === '0' || peopleValue === '',
+          ...calculateTipAndTotal(prevState.bill, peopleValue, prevState.percentage),
         };
       }
-      case UPDATE_PEOPLE:
-        return {
-          ...prevState,
-          people: payload,
-        }
       case RESET:
         return {
           ...initialState,
-        }
+        };
       default:
         throw new Error('Invalid action type');
     }
