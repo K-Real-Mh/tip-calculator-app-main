f3bd814add6c925bd340e0dc6f782e04e92ab361
Kirill Makhov <mmahovkirill@gmail.com>
2023-04-28 22:46:10 +0300

    refactor: add blocks

--- src/containers/TipCalculator/TipCalculator.jsx
+++ src/containers/TipCalculator/TipCalculator.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import StateProvider from './context/StateProvider';
 import './TipCalculator.css';
+import BlockWrapper from './containers/BlockWrapper';
+import { BLOCK_TYPES } from './consts';
 
 function TipCalculator() {
   return (
     <StateProvider>
-      <div className="TipCalculator">Tip Calculator</div>
+      <div className="TipCalculator">
+        <div className="TipCalculator__form">
+          <BlockWrapper type={BLOCK_TYPES.BILL} />
+          <BlockWrapper type={BLOCK_TYPES.PERCENTAGE} />
+          <BlockWrapper type={BLOCK_TYPES.PEOPLE} />
+        </div>
+        <div className="TipCalculator__display">
+          <div className="TipCalculator__strings">
+            <BlockWrapper type={BLOCK_TYPES.TOTAL} />
+            <BlockWrapper type={BLOCK_TYPES.TIP} />
+          </div>
+          <BlockWrapper type={BLOCK_TYPES.RESET} />
+        </div>
+      </div>
     </StateProvider>
   );
 }
