5374c6b8db48249096e72e9f3a47348ca5e5faae
Kirill Makhov <mmahovkirill@gmail.com>
2023-04-29 01:54:30 +0300

    feat: add decreasing font size from the price length

--- src/containers/TipCalculator/components/PriceString/PriceString.jsx
+++ src/containers/TipCalculator/components/PriceString/PriceString.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './PriceString.css';
 
-function PriceString({className, title, price}) {
+function PriceString({ className, title, price }) {
   return (
-  <div className={`PriceString ${className || ''}`}>
-    <div className='PriceString__info'>
-      <div className='PriceString__title'>{title}</div>
-      <div className='PriceString__subtitle'>/ person</div>
+    <div className={`PriceString ${className || ''}`}>
+      <div className="PriceString__info">
+        <div className="PriceString__title">{title}</div>
+        <div className="PriceString__subtitle">/ person</div>
+      </div>
+      <div
+        className={`PriceString__price ${price.length > 8 ? 'PriceString__price--s' : ''} ${
+          price.length > 12 ? 'PriceString__price--xs' : ''
+        }`}
+      >
+        ${price}
+      </div>
     </div>
-    <div className='PriceString__price'>${price}</div>
-  </div>
-)}
+  );
+}
 
 export default PriceString;
